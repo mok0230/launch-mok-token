3d8fba9ea879df959f906327da068f3bf80faef9
mok <mok0230@protonmail.com>
2021-11-09 11:05:57 -0800

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
+
+// TO DO: Copy-paste your Alchemy HTTP Key from Step #1
+const ALCHEMY_API_HTTP_KEY = '';
+
+// TO DO: Copy-paste your MetaMask Test Account Rinkeby Private Key **MAKE SURE YOU **
+const RINKEBY_PRIVATE_KEY = '';
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: '${ALCHEMY_API_HTTP_KEY}',
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
+    }
+  }
 };
