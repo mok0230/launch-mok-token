0559d70fd2983125f329ec85a7215912e8bdab9a
mok <mok0230@protonmail.com>
2021-11-09 11:13:48 -0800

    update config to reference environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-waffle");
 
 // TO DO: Copy-paste your Alchemy HTTP Key from Step #1
-const ALCHEMY_API_HTTP_KEY = '';
+const ALCHEMY_API_HTTP_KEY = process.env.ALCHEMY_API_HTTP_KEY;
 
 // TO DO: Copy-paste your MetaMask Test Account Rinkeby Private Key **MAKE SURE YOU **
-const RINKEBY_PRIVATE_KEY = '';
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
-  solidity: '0.8.0',
+  solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: '${ALCHEMY_API_HTTP_KEY}',
-      accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
-    }
+      url: `${ALCHEMY_API_HTTP_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    } 
   }
 };
